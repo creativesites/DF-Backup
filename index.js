a79053438b1df0102bd2c0d681c64f2cc4159181
Winston <46081328+creativesites@users.noreply.github.com>
2022-11-02 14:00:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ async function run() {
             //await page1.waitForTimeout(10000);
             console.log('BACKUP')
             try {
-                for(let el = 1; el < 48; el ++) {
+                for(let el = 7; el < 50; el ++) {
                     console.log(`Backing up ${el}`)
                     //await UpdateSheet("G4", `Backup ${el} Started`)
                     //select select agent button
@@ -465,8 +465,8 @@ async function Rename() {
     }
     
 }
-//run()
+run()
 //Rename()
 //UploadFiles()
- const result = findRemoveSync('./backup', { extensions: ['.zip'] })
+// const result = findRemoveSync('./backup', { extensions: ['.zip'] })
 // console.log(result)
